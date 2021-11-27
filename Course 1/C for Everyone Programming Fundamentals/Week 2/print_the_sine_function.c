0987644352b0c0e70c3365daa590d85449fc7cdb
Pramatma Vishwakarma <vpramatma@gmail.com>
2021-11-27 09:12:53 +0530

    first commit

--- Course 1/C for Everyone Programming Fundamentals/Week 2/print_the_sine_function.c
+++ Course 1/C for Everyone Programming Fundamentals/Week 2/print_the_sine_function.c
@@ -1,12 +1,15 @@
-#include <math.h>
+/*SIN function in the C programming language*/
+
+#include <math.h> // MATH library functions to calculate sin value
 #include <stdio.h>
-int main(void)
+int main()
 {
     double sinValue, X;
-    printf("Enter the value of calculate sine:\n");
-    scanf("%f", &X);
+    printf("Enter the value to calculate sine:\n");
+    scanf("%lf", &X);
 
     sinValue =sin(X);
 
     printf("\n The sine value of %lf = %lf\n", X, sinValue);
+    return 0;
 }
